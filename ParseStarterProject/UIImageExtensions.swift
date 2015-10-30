dc0448ce99105e0c922e5426a29a4c60898ae260
its-micah <mlanier86@gmail.com>
2015-10-30 17:47:27 -0500

    add imageWithColor func for tab bar icons

--- ParseStarterProject/UIImageExtensions.swift
+++ ParseStarterProject/UIImageExtensions.swift
@@ -19,4 +19,23 @@ extension UIImage {
     var lowQualityJPEGNSData: NSData     { return UIImageJPEGRepresentation(self, 0.25)! }
     var lowestQualityJPEGNSData:NSData   { return UIImageJPEGRepresentation(self, 0.0)!  }
 
+    func imageWithColor(tintColor: UIColor) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
+
+        let context = UIGraphicsGetCurrentContext()! as CGContextRef
+        CGContextTranslateCTM(context, 0, self.size.height)
+        CGContextScaleCTM(context, 1.0, -1.0);
+        CGContextSetBlendMode(context, .Normal)
+
+        let rect = CGRectMake(0, 0, self.size.width, self.size.height) as CGRect
+        CGContextClipToMask(context, rect, self.CGImage)
+        tintColor.setFill()
+        CGContextFillRect(context, rect)
+
+        let newImage = UIGraphicsGetImageFromCurrentImageContext() as UIImage
+        UIGraphicsEndImageContext()
+
+        return newImage
+    }
+
 }
