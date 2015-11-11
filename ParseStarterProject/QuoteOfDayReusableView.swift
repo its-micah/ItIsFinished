e53c48de462cfb5613314288e3a2a8462b479dd1
its-micah <mlanier86@gmail.com>
2015-11-11 11:10:34 -0600

    add quote of day view

---
+++ ParseStarterProject/QuoteOfDayReusableView.swift
@@ -0,0 +1,29 @@
+//
+//  QuoteOfDayReusableView.swift
+//  ParseStarterProject-Swift
+//
+//  Created by Micah Lanier on 11/9/15.
+//  Copyright © 2015 Parse. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class QuoteOfDayReusableView: UICollectionReusableView {
+
+    @IBOutlet weak var quoteOfDayLabel: UILabel!
+
+    func configureWithQuote() {
+
+        let quote = MadLibManager.sharedInstance.getQuoteOfDay()
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 8
+
+        let attrString = NSMutableAttributedString(string: quote)
+        attrString.addAttribute(NSParagraphStyleAttributeName, value:paragraphStyle, range:NSMakeRange(0, attrString.length))
+
+        quoteOfDayLabel.attributedText = attrString
+    }
+
+
+}
