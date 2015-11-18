e796a7611720dbfd818415bbf557c27b4f2a7941
its-micah <mlanier86@gmail.com>
2015-11-17 22:04:04 -0600

    rework Twitter Login

--- ParseStarterProject/LoginViewController.swift
+++ ParseStarterProject/LoginViewController.swift
@@ -30,42 +30,31 @@ class LoginViewController: UIViewController {
         }
     }
 
-    @IBAction func onButtonTapped(sender: AnyObject) {
-        let user = User()
-        user.username = usernameField.text
-        user.password = passwordField.text
-        
-        User.logInWithUsernameInBackground(user.username!, password: user.password!) { (var user: PFUser?, error: NSError?) -> Void in
-            if user != nil {
-                SVProgressHUD.show()
-                PFQuery.clearAllCachedResults()
-                user = MadLibManager.sharedInstance.currentUser
-                user?.objectId = user?.valueForKey("objectId") as? String
-                self.setUserImage()
-                self.dismissViewControllerAnimated(true, completion: nil)
-            } else {
-                let alertView = UIAlertController(title: "WOAH NOW", message: "You don't have an account, please register.", preferredStyle: .Alert)
-                alertView.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
-                self.presentViewController(alertView, animated: true, completion: nil)
-            }
-        }
-
-       
-    }
 
     @IBAction func onTwitterButtonTapped(sender: AnyObject) {
 
-        PFTwitterUtils.logInWithBlock {
-            (user: PFUser?, var error: NSError?) -> Void in
-            if var user = user {
+        PFTwitterUtils.logInWithBlock {(user: PFUser?, error: NSError?) -> Void in
+
+            if let user = user {
+
                 if user.isNew {
-                    SVProgressHUD.show()
-                    print("User signed up and logged in with Twitter!")
-                    let newUser = MadLibManager.sharedInstance.currentUser
+
+                    let userID = PFTwitterUtils.twitter()?.userId
                     let twitterUsername = PFTwitterUtils.twitter()?.screenName
-                    newUser!.username = twitterUsername
-    
-                    let requestString = ("https://api.twitter.com/1.1/users/show.json?screen_name=" + twitterUsername!)
+                    user.username = twitterUsername
+
+                    var requestString = ("https://api.twitter.com/1.1/users/show.json?")
+
+                    if userID!.characters.count > 0 {
+                        requestString = requestString.stringByAppendingString("user_id=" + userID!)
+                    } else if (twitterUsername?.characters.count > 0) {
+                        requestString = requestString.stringByAppendingString("screen_name=" + twitterUsername!)
+                    } else {
+                        let alertView = UIAlertController(title: "WOAH NOW", message: "Something went wrong", preferredStyle: .Alert)
+                        alertView.addAction(UIAlertAction(title: "OK, Whatever", style: .Default, handler: nil))
+                        self.presentViewController(alertView, animated: true, completion: nil)
+                        return;
+                    }
 
                     let verify: NSURL = NSURL(string: requestString)!
 
@@ -73,44 +62,52 @@ class LoginViewController: UIViewController {
 
                     PFTwitterUtils.twitter()?.signRequest(request)
 
-                    var response: NSURLResponse?
+                    NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(),
+                        completionHandler:{(response: NSURLResponse?, data: NSData?, error: NSError?) -> Void in
+                            if error == nil {
+                                SVProgressHUD.show()
+                                do {
+                                    let newUser = user as! User
+                                    let result = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments)
+                                    let urlString = result.objectForKey("profile_image_url_https") as! String
+                                    let hiResUrlString = urlString.stringByReplacingOccurrencesOfString("_normal", withString: "", options: NSStringCompareOptions.LiteralSearch, range: nil)
+                                    let twitterPhotoUrl = NSURL(string: hiResUrlString)
+                                    let imageData = NSData(contentsOfURL: twitterPhotoUrl!)
+                                    let twitterImage = UIImage(data: imageData!)
+                                    let compressedImage = twitterImage?.highQualityJPEGNSData
+                                    let imageFile = PFFile(name: "profilePic.jpg", data: compressedImage!)
+                                    newUser.profilePicture = imageFile
 
-                    do {
-                        let data: NSData = try NSURLConnection.sendSynchronousRequest(request, returningResponse: &response)
-                        let result = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments)
-                        let urlString = result.objectForKey("profile_image_url_https") as! String
-                        let hiResUrlString = urlString.stringByReplacingOccurrencesOfString("_normal", withString: "", options: NSStringCompareOptions.LiteralSearch, range: nil)
+                                    newUser.saveInBackgroundWithBlock({ (success: Bool, error: NSError?) -> Void in
+                                        if success {
+                                            MadLibManager.sharedInstance.currentUser = newUser
+                                            MadLibManager.sharedInstance.currentUser!.convertToImageWithPFFile(imageFile)
+                                            SVProgressHUD.dismiss()
+                                            self.dismissViewControllerAnimated(true, completion: nil)
+                                        }
+                                    })
 
+                                }
 
-                        let twitterPhotoUrl = NSURL(string: hiResUrlString)
-                        let imageData = NSData(contentsOfURL: twitterPhotoUrl!)
-
-                        let imageFile = PFFile(name: "profilePic.jpg", data: imageData!)
-                        newUser!.profilePicture = imageFile
-                        newUser!.convertToImageWithPFFile(imageFile)
-
-                        newUser!.saveInBackgroundWithBlock({ (success: Bool, error: NSError?) -> Void in
-                            if success {
-                                self.dismissViewControllerAnimated(true, completion: nil)
+                                catch {
+                                    print(error)
+                                }
+                                
                             }
-                        })
-
-                    } catch {
-                        print("error with synchronouss request")
-                    }
+                    })
 
 
                 } else {
                     print("User logged in with Twitter!")
                     self.dismissViewControllerAnimated(true, completion: nil)
+                    MadLibManager.sharedInstance.currentUser = (user as! User)
+
                 }
 
-            } else {
-
-                print("Uh oh. The user cancelled the Twitter login.")
-
             }
+
         }
+
 
     }
 
