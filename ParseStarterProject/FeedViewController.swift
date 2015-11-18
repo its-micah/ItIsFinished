7a7d8cebfd657f097c41ebe366a22f9fb6a94bd5
its-micah <mlanier86@gmail.com>
2015-11-17 22:04:28 -0600

    Add refresh to main feed

--- ParseStarterProject/FeedViewController.swift
+++ ParseStarterProject/FeedViewController.swift
@@ -13,83 +13,137 @@ import Parse
 import ParseTwitterUtils
 
 class FeedViewController: UIViewController,
-UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, FeedCellProtocol {
+UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, FeedCellProtocol, NetworkProtocol {
     
     @IBOutlet weak var feedCollectionView: UICollectionView!
     @IBOutlet weak var emptyFeedView: UIView!
-    var feedArray: Array<PFObject> = [PFObject]()
-    var postUser: User?
+    let tapRecog = UITapGestureRecognizer()
+    let refreshControl = UIRefreshControl()
+    var isRefreshing = false
+    var feedArray = [PFObject]()
+    var postUser: User? = nil
     var loaded: Bool? = false
+    var selectedUser: User? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
         feedCollectionView.delegate = self
         feedCollectionView.collectionViewLayout as! UICollectionViewFlowLayout
+
+        refreshControl.addTarget(self, action: Selector("refresh"), forControlEvents: UIControlEvents.ValueChanged)
+        feedCollectionView.addSubview(refreshControl)
+
+        NetworkManager.sharedInstance.delegate = self
+
+        let headerNib = UINib(nibName: "QuoteOfDayView", bundle: nil)
+        feedCollectionView.registerNib(headerNib, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "quoteOfDay")
     }
 
     
     override func viewWillAppear(animated: Bool) {
-        loadLibs()
+        refresh()
         self.emptyFeedView.alpha = 0
     }
 
-    func loadLibs() {
-        let query = PFQuery(className: "MadLib")
-        query.orderByDescending("createdAt")
-        query.cachePolicy = .CacheThenNetwork
-        query.findObjectsInBackgroundWithBlock { (objects: [PFObject]?, error: NSError?) -> Void in
-            if error == nil && objects?.count > 0 && objects?.count != self.feedArray.count {
-                print("received \(objects?.count) madlibs")
-                self.feedArray.removeAll()
-                for var madLib in objects! {
-                    self.feedArray.append(madLib)
-                }
-                self.feedCollectionView.reloadData()
-                SVProgressHUD.dismiss()
-            } else {
-               self.animateEmptyFeedView()
-            }
-        }
-    
+
+    func refresh() {
+        NetworkManager.sharedInstance.loadFollowingLibs()
     }
+
+    func endRefresh() {
+        print("refreshing ended")
+    }
+
+    func finishedGatheringData() {
+
+        feedArray = NetworkManager.sharedInstance.feedArray
+
+        if feedArray.count > 0 {
+            emptyFeedView.alpha = 0
+            self.feedCollectionView.reloadData()
+            refreshControl.endRefreshing()
+        } else {
+            animateEmptyFeedView()
+        }
+
+
+    }
+
     
     func animateEmptyFeedView() {
         UIView.animateWithDuration(0.4, animations: { () -> Void in
             self.emptyFeedView.alpha = 1.0
         })
     }
-    
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-        return CGSizeMake(feedCollectionView.frame.size.width/1.05, feedCollectionView.frame.size.height/1.8)
-    }
-    
-    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return feedArray.count
-    }
-    
-    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        var cell = feedCollectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as? FeedCell
-        if cell == nil {
-            cell = FeedCell()
-        }
-        cell?.userImageView.image = UIImage(named: "profile")
-        cell!.delegate = self
-        cell!.configureWithPost(feedArray[indexPath.row])
-        postUser = feedArray[indexPath.row].objectForKey("user") as? User
-        cell!.configureWithUser(postUser!)
-        cell!.addTapRecog()
-        return cell!
-    }
 
     func showProfile() {
-        let selectedUser = postUser
-        selectedUser?.convertToImageWithPFFile(selectedUser?.profilePicture)
+        selectedUser!.convertToImageWithPFFile(selectedUser!.profilePicture)
         let profileVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("ProfileViewController") as! ProfileViewController
         profileVC.previousViewController = "FeedViewController"
         profileVC.user = selectedUser
         self.navigationController?.pushViewController(profileVC, animated: true)
-        
     }
+
+    func showSharingWithImageAndText(image: UIImage, text: String) {
+        let activityViewController : UIActivityViewController = UIActivityViewController(
+            activityItems: [text, image],
+            applicationActivities: nil)
+        let presentationController = activityViewController.popoverPresentationController
+        presentationController?.sourceView = self.view
+        presentationController?.sourceRect = CGRect(
+            origin: CGPointZero,
+            size: CGSize(width: self.view.frame.width / 1.2, height: self.view.frame.height / 1.2))
+
+        self.presentViewController(activityViewController, animated: true, completion: nil)
+    }
+
+    func showCreateLib() {
+        print("tapped")
+        let createLibVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("CreateLibVC") as! CreateLibViewController
+        createLibVC.libText = MadLibManager.sharedInstance.quoteOfDay
+        self.navigationController?.pushViewController(createLibVC, animated: true)
+    }
+
+
+    ///-------------------------
+    /// MARK: CollectionView Delegate Methods
+    ///-------------------------
+
+
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSizeMake(feedCollectionView.frame.size.width/1.05, 200)
+    }
+
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSizeMake(feedCollectionView.frame.size.width/1.05, feedCollectionView.frame.size.height/1.8)
+    }
+
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return feedArray.count
+    }
+
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = feedCollectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as! FeedCell
+        cell.userImageView.image = UIImage(named: "profile")
+        cell.userNameLabel.text = ""
+        cell.userImageView.image = nil
+        cell.delegate = self
+        cell.postUser = nil
+        cell.postUser = feedArray[indexPath.row].objectForKey("user") as? User
+        cell.configureWithPost(feedArray[indexPath.row])
+        cell.addTapRecog()
+        cell.setNeedsLayout()
+        return cell
+    }
+
+    func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
+        let reusableView = collectionView.dequeueReusableSupplementaryViewOfKind(UICollectionElementKindSectionHeader, withReuseIdentifier: "quoteOfDay", forIndexPath: indexPath) as! QuoteOfDayReusableView
+        reusableView.configureWithQuote()
+        tapRecog.addTarget(self, action: Selector("showCreateLib"))
+        reusableView.addGestureRecognizer(tapRecog)
+        return reusableView
+    }
+
     
     
 }
